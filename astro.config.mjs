d1bb5a491e2002bb7112098d40b5582eafd36232
Rhilip <rhilipruan@gmail.com>
2026-06-25 21:44:30 +0800

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://status.bsgun.cn",
-  base: "/",
+  site: "https://pt-plugins.github.io",
+  base: "/monitor",
   srcDir: "./src",
   outDir: "./dist",
   publicDir: "./public",
